e729d39d22c20ed71d1df8ad43f7a579160543d4
tobegram <tom_beyer@gmx.net>
2022-03-03 13:26:50 +0100

    documentation, install autopep8

--- grocery_list.py
+++ grocery_list.py
@@ -27,10 +27,15 @@ groceries = [
 ]
 
 groceries = sorted(groceries)
-
+# create a connection to the grocery_list.db
+# create an cursor object to interact with database 
 connection = sqlite3.connect("grocery_list.db")
 cursor = connection.cursor()
 
+# Create a table in grocery_list.db with 
+# 1. column id index and
+# 2. column groceries
+# loop will add every item in grocery list into db table
 cursor.execute("CREATE TABLE groceries (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)")
 for i in range(len(groceries)):
 	cursor.execute("INSERT INTO groceries (name) values (?)",[groceries[i]])
